2c38c160dc2f4631415b67007aceb36344e803b9
karber9 <150918920+karber9@users.noreply.github.com>
2026-05-28 12:01:58 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
+'''basic calculator'''
 def add(a: int, b: int) -> int:
+  '''suma'''
   return a+b
 def subtract(a: int, b: int) -> int:
+  '''roznica'''
   return a-b
 def multiply(a: int, b: int) -> int:
+  '''mnozenie'''
   return a*b
 def divide(a: int, b: int) -> float:
+  '''dzielenie'''
   return a/b

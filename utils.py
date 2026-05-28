a987881900a6569edc780f4a8c375f3725e911a8
karber9 <bernikerkarolina@gmail.com>
2026-05-28 12:04:20 +0200

    ci cd workflow

--- utils.py
+++ utils.py
@@ -1,13 +1,21 @@
-'''basic calculator'''
+"""basic calculator"""
+
+
 def add(a: int, b: int) -> int:
-  '''suma'''
-  return a+b
+    """suma"""
+    return a + b
+
+
 def subtract(a: int, b: int) -> int:
-  '''roznica'''
-  return a-b
+    """roznica"""
+    return a - b
+
+
 def multiply(a: int, b: int) -> int:
-  '''mnozenie'''
-  return a*b
+    """mnozenie"""
+    return a * b
+
+
 def divide(a: int, b: int) -> float:
-  '''dzielenie'''
-  return a/b
+    """dzielenie"""
+    return a / b
